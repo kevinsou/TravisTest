352d0d21fbf05aeb4e7761c6346e3637ef5a4935
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 22:46:59 -0700

    Create PlayerShipTest.java

---
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.a1;
+
+
+import com.codename1.testing.AbstractTest;
+import com.codename1.testing.TestUtils;
+
+/**
+ *
+ * @author shannah
+ */
+public class PlayerShipTest extends AbstractTest {
+
+    public boolean runTest() throws Exception {
+        TestUtils.assertBool(1+2==3, "One plus 2 should be 3");
+        return true;
+    }
+    
+}
