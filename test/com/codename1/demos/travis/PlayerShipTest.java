6e30f986e0f4a66152b23c93caf5f14d3e8e5753
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 23:26:25 -0700

    Update PlayerShipTest.java

--- test/com/codename1/demos/travis/PlayerShipTest.java
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -3,23 +3,15 @@ package com.mycompany.a1;
 
 import com.codename1.testing.AbstractTest;
 import com.codename1.testing.TestUtils;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 
 public class PlayerShipTest extends AbstractTest {
-    
-        @Test
-    public void test1 () {
-    	PlayerShip tester = new PlayerShip ();
-    	tester.steerLeft();
-    	assertEquals(15,tester.getDirection());
-    }
 
-//     public boolean runTest() throws Exception {
-//         PlayerShip tester = new PlayerShip();
-//         tester.steerLeft();
-//         TestUtils.assertBool(tester.getDirection()==15, "the direction should be 15");
-//         return true;
-//     }
+
+    public boolean runTest() throws Exception {
+         PlayerShip tester = new PlayerShip();
+         tester.steerLeft();
+         TestUtils.assertBool(tester.getDirection()==15, "the direction should be 15");
+         return true;
+     }
     
 }
