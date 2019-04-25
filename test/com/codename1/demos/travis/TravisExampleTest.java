ab2f95b2ec6bbabebd4bb9d3fe948addd0180a58
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 22:25:36 -0700

    Update TravisExampleTest.java

--- test/com/codename1/demos/travis/TravisExampleTest.java
+++ test/com/codename1/demos/travis/TravisExampleTest.java
@@ -1,30 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.codename1.demos.travis;
+package com.mycompany.a1;
 
-import com.codename1.testing.AbstractTest;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
+class PlayerShipTest{
 
+	@Test
+	void SteerLeftTest() {
+		PlayerShip tester = new PlayerShip();
+		tester.steerLeft();
+		assertEquals(15,tester.getDirection());
+	}
 
-/**
- *
- * @author shannah
- */
-public class TravisExampleTest extends AbstractTest {
-    
-    public TravisExampleTest() {
-    }
-
-    
-    public void testSomeMethod() {
-    }
-
-    @Override
-    public boolean runTest() throws Exception {
-        return true;
-    }
-    
 }
