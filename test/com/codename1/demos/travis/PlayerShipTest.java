2291d4ebc9abad9a6fe9c142ded8a135dd03efe3
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 21:56:05 -0700

    Update PlayerShipTest.java

--- test/com/codename1/demos/travis/PlayerShipTest.java
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -2,6 +2,7 @@ package com.mycompany.a1;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
+import com.mycompany.a1.PlayerShip;
 
 class PlayerShipTest extends PlayerShip{
 
