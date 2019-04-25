f4881386679a3c682d46953a056590da87e72f24
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 22:50:45 -0700

    Update PlayerShipTest.java

--- test/com/codename1/demos/travis/PlayerShipTest.java
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -4,14 +4,12 @@ package com.mycompany.a1;
 import com.codename1.testing.AbstractTest;
 import com.codename1.testing.TestUtils;
 
-/**
- *
- * @author shannah
- */
+
 public class PlayerShipTest extends AbstractTest {
 
     public boolean runTest() throws Exception {
         PlayerShip tester = new PlayerShip();
+        tester.steerLeft();
         TestUtils.assertBool(1+2==3, "One plus 2 should be 3");
         return true;
     }
