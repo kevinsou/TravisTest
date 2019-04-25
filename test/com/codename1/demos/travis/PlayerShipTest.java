6f51d025a201ced40fd01be344766220231c734b
Kevin Sou <edskevin0008@gmail.com>
2019-04-24 22:17:42 -0700

    Update PlayerShipTest.java

--- test/com/codename1/demos/travis/PlayerShipTest.java
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -6,24 +6,19 @@
 package com.codename1.demos.travis;
 
 import com.codename1.testing.AbstractTest;
-
+import com.codename1.testing.TestUtils;
 
 
 /**
  *
  * @author shannah
  */
-public class TravisExampleTest extends AbstractTest {
-    
-    public TravisExampleTest() {
-    }
+public class PlayerShipTest extends AbstractTest {
 
-    
-    public void testSomeMethod() {
-    }
-
-    @Override
     public boolean runTest() throws Exception {
+        PlayerShip tester = new PlayerShip();
+		tester.steerLeft();
+        TestUtils.assertBool(15, tester.getDirection());
         return true;
     }
     
