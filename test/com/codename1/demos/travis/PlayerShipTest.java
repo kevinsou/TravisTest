dbd97e260a442455e18e9fd9075773d5b859eed6
Kevin Sou <edskevin0008@gmail.com>
2019-05-01 14:26:30 -0700

    Update PlayerShipTest.java

--- test/com/codename1/demos/travis/PlayerShipTest.java
+++ test/com/codename1/demos/travis/PlayerShipTest.java
@@ -14,10 +14,7 @@ public class PlayerShipTest extends AbstractTest {
     public boolean runTest() throws Exception {
          PlayerShip tester = new PlayerShip();
          tester.steerLeft();
-         TestUtils.assertBool(tester.getDirection()==15, "the direction should be 15");
-         tester = new PlayerShip();
-         tester.steerRight();
-         TestUtils.assertBool(tester.getDirection()==-15, "the direction should be -15");
+         TestUtils.assertBool(tester.getDirection()==10, "the direction should be 10");
          return true;
      }
     
