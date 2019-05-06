5fa23694788793454fcff1634563a4562cd8ae70
Kevin Sou <edskevin0008@gmail.com>
2019-05-05 19:36:08 -0700

    Create PlayerShipSteerRightTest.java

---
+++ test/com/codename1/demos/travis/PlayerShipSteerRightTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.a1;
+
+
+import com.codename1.testing.AbstractTest;
+import com.codename1.testing.TestUtils;
+
+public class PlayerShipSteerRightTest extends AbstractTest {
+
+
+/**
+ *
+ * 
+ */
+    public boolean runTest() throws Exception {
+         PlayerShip tester = new PlayerShip();
+         tester.steerRight();
+         TestUtils.assertBool(tester.getDirection()==344, "the direction should be 344");
+         tester.steerRight();
+         TestUtils.assertBool(tester.getDirection()==329, "the direction should be 329");
+         return true;
+     }
+    
+    
+}
