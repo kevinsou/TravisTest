0a037be554a472bcbd2ebf88b36a58829e6bdd0d
Kevin Sou <edskevin0008@gmail.com>
2019-05-07 10:19:37 -0700

    Update PlayerShip.java

--- src/com/mycompany/a1/PlayerShip.java
+++ src/com/mycompany/a1/PlayerShip.java
@@ -21,18 +21,18 @@ public class PlayerShip extends Ships implements ISteerable {
 	@Override
 	public void steerLeft() {
 		// TODO Auto-generated method stub
-		this.direction=direction+15;
-		if(this.direction>359) {
-			this.direction=direction-359;
-		}
+		//this.direction=direction+15;
+		//if(this.direction>359) {
+		//	this.direction=direction-359;
+		//}
 	}
 	@Override
 	public void steerRight() {
 		// TODO Auto-generated method stub
-		this.direction=direction-15;
-		if(this.direction<0) {
-			this.direction=direction+359;
-		}
+		//this.direction=direction-15;
+		//if(this.direction<0) {
+		//	this.direction=direction+359;
+		//}
 	}
 	public String toString() {
 		return ("PLAYERSHIP: Location = "+getX()+","+getY()+
