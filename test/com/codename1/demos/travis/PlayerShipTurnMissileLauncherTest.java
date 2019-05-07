7723850b69396dd99affe9d45cbc3c6ec9b0e24b
Kevin Sou <edskevin0008@gmail.com>
2019-05-07 09:54:52 -0700

    Update PlayerShipTurnMissileLauncherTest.java

--- test/com/codename1/demos/travis/PlayerShipTurnMissileLauncherTest.java
+++ test/com/codename1/demos/travis/PlayerShipTurnMissileLauncherTest.java
@@ -1,1 +1,25 @@
+package com.mycompany.a1;
 
+
+import com.codename1.testing.AbstractTest;
+import com.codename1.testing.TestUtils;
+
+public class PlayerShipTurnMissileLauncherTest extends AbstractTest {
+
+
+/**
+ *
+ * 
+ */
+    public boolean runTest() throws Exception {
+         PlayerShip tester = new PlayerShip();
+         tester.steerLeft();
+         TestUtils.assertBool(tester.getDirection()==15, "the direction is" + tester.getDirection() + "when it should be 15");
+         tester.setDirection(359);
+         tester.steerLeft();
+         TestUtils.assertBool(tester.getDirection()==15, "the direction is" + tester.getDirection()+ "when it should be 15");
+         return true;
+     }
+    
+    
+}
